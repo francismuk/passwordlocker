b9ea16d0366667b03e89ef9e5f87090a1c589d5a
Francis Mukuha <mukuha58@gmail.com>
2019-06-13 12:53:55 +0300

    add a new file with class User

---
+++ user.py
@@ -0,0 +1,11 @@
+class User:
+    
+    user_list = []
+
+    def __init__(self,f_name,s_name,password):
+    
+        self.f_name = f_name
+        self.s_name = s_name
+        self.password = password
+
+
