26b2b9482dc13881a71d8ff92c1f60979f43557d
Francis Mukuha <mukuha58@gmail.com>
2019-06-14 08:14:02 +0300

    add teardown and save multiple users

--- user_test.py
+++ user_test.py
@@ -19,11 +19,11 @@ class TestUser(unittest.TestCase):
         
         self.new_user = User("Francis", "Mukuha", "1234") # create login credentials
 
-    # def tearDown(self):
-    #     '''
-    #     teardown method that does clean up after each test case has run
-    #     '''
-    #     User.user_list = []
+    def tearDown(self):
+        '''
+        teardown method that does clean up after each test case has run
+        '''
+        User.user_list = []
         
     def test_init(self):
         
@@ -43,15 +43,15 @@ class TestUser(unittest.TestCase):
         self.new_user.save_user()
         self.assertEqual(len(User.user_list), 1)
         
-    # def test_save_multiple_user(self):
+    def test_save_multiple_user(self):
         
-    #     '''
-    #     test case to test if multiple users can all create accounts and login with their credentials
-    #     '''
-    #     self.new_user.save_user()
-    #     test_user = User("Test","user","1998") #new user credentials
-    #     test_user.save_user()
-    #     self.assertEqual(len(User.user_list), 2)
+        '''
+        test case to test if multiple users can all create accounts and login with their credentials
+        '''
+        self.new_user.save_user()
+        test_user = User("Test","user","1998") #new user credentials
+        test_user.save_user()
+        self.assertEqual(len(User.user_list), 2)
 
 
 if __name__ == "__main__":
