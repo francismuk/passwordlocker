4c5b8496ed19fcdde244c2418c6d7e04ecd56bbf
Francis Mukuha <mukuha58@gmail.com>
2019-06-16 17:56:41 +0300

    add function to generate passwords 8 and 6 size

--- run.py
+++ run.py
@@ -29,7 +29,19 @@ def verify_user(first_name,password):
 	checking_user = Credential.check_user(first_name,password)
 	return checking_user
 
+def generate_password():
+	'''
+	Function to generate a password automatically
+	'''
+	gen_pass = Credential.generate_password()
+	return gen_pass
 
+def generate_password_six():
+	'''
+	Function to generate a six letter password automatically
+	'''
+	gen_pass_six = Credential.generate_password_six()
+	return gen_pass_six
 
 
 def create_credentials(user_name,password):
@@ -88,7 +100,79 @@ elif short_code == 'cu':
 			print("\n")
 			print(f'New Account: {first_name} {last_name} login password: {password}')
 elif short_code == 'du':
-			
+			print('\n')
+			print('To login, enter your first name and password:')
+			f_name = input()
+			password = str(input())
+			user_exists = verify_user(user_name,password)
+			if user_exists == user_name:
+				print(" ")
+				print(f'Welcome {user_name}. Please choose an option to continue.')
+				print(' ')
+				while True:
+					print("-"*60)
+					print('Navigation codes: \n cc-Create a Credential \n dc-Display Credentials \n copy-Copy Password \n ex-Exit')
+					short_code = input('Enter a choice: ').lower().strip()
+					print("-"*60)
+					if short_code == 'ex':
+						print(" ")
+						print(f'Goodbye {user_name}')
+						break
+					elif short_code == 'cc':
+						print(' ')
+						print('Enter your credential details:')
+						site_name = input('Enter the site\'s name- ').strip()
+						account_name = input('Enter your account\'s name - ').strip()
+						while True:
+							print(' ')
+							print("-"*60)
+							print('Please choose an option for entering a password: \n ep-enter existing password \n gp-generate a password \n ex-exit')
+							psw_choice = input('Enter an option: ').lower().strip()
+							print("-"*60)
+							if psw_choice == 'ep':
+								print(" ")
+								password = input('Enter your password: ').strip()
+								break
+							elif psw_choice == 'gp':
+								password = generate_password()
+								break
+							elif psw_choice == 'ex':
+								break
+							else:
+								print('Oops! Wrong option entered. Try again.')
+						save_credential(create_credential(user_name,site_name,account_name,password))
+						print(' ')
+						print(f'Credential Created: Site Name: {site_name} - Account Name: {account_name} - Password: {password}')
+						print(' ')
+					elif short_code == 'dc':
+						print(' ')
+						if display_credentials(user_name):
+							print('Here is a list of all your credentials')
+							print(' ')
+							for credential in display_credentials(user_name):
+								print(f'Site Name: {credential.site_name} - Account Name: {credential.account_name} - Password: {credential.password}')
+							print(' ')	
+						else:
+							print(' ')
+							print("You don't seem to have any credentials saved yet")
+							print(' ')
+					elif short_code == 'copy':
+						print(' ')
+						chosen_site = input('Enter the site name for the credential password to copy: ')
+						copy_credential(chosen_site)
+						print('')
+					else:
+						print('Oops! Wrong option entered. Try again.')
+
+			else: 
+				print(' ')
+				print('Oops! Wrong details entered. Try again or Create an Account.')		
+		
+else:
+			print("-"*60)
+			print(' ')
+			print('Oops! Wrong option entered. Try again.')
+        
     
 if __name__ == '__main__':
     main()
